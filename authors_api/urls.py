3bab34716e21627013fd89fc9b4d57c50b2f5fbd
echeadle <echeadle@gmail.com>
2022-08-01 20:03:47 -0600

    Create multiple containers for application.

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core_apps/common/apps.py
+++ core_apps/common/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 class CommonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core_apps.common'
-    verbose_name = _('Commone')
+    verbose_name = _('Common')
